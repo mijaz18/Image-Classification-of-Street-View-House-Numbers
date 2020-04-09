e1ac2a3d44d3e734adc7cbc55bdcbea0ec09158b
Muhammad Usama Ijaz <mijaz@u.rochester.edu>
2020-04-09 02:16:39 -0400

    checking lstm

--- network.py
+++ network.py
@@ -110,7 +110,7 @@ class Decoder(nn.Module):
         #self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
         # self.lstm = LSTM(embed_size,h0,h1, hidden_size)
         # print(inspect.getargspec(LSTM.__init__))
-        self.lstm = LSTM(embed_size,hidden_size)
+        self.lstm = LSTM(self,embed_size,hidden_size)
 
         
         self.linear = nn.Linear(hidden_size, vocab_size) # project the outputs from LSTM to vocabulary space
