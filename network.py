fdcab3a76f7c332d32b428a2667e94006c648d5c
Muhammad Usama Ijaz <mijaz@u.rochester.edu>
2020-04-09 02:54:14 -0400

    checking lstm

--- network.py
+++ network.py
@@ -110,7 +110,7 @@ class Decoder(nn.Module):
         #self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
         states = (autograd.Variable(torch.zeros(1, embed_size, self.hidden_size).cuda()),
                   autograd.Variable(torch.zeros(1, embed_size, self.hidden_size).cuda()))
-        self.lstm = LSTM(embed_size,states,hidden_size)
+        self.lstm = LSTM(states,hidden_size)
 
         
         self.linear = nn.Linear(hidden_size, vocab_size) # project the outputs from LSTM to vocabulary space
